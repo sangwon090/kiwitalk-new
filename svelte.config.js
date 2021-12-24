fd08f0cbcffb91665831ec705dc3d5ac59a56769
Sangwon Ryu <bdboy090@naver.com>
2021-12-25 01:55:57 +0900

    Install tauri

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'app.html'
+		}),
+		ssr: false,
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
